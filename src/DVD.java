84b469250933135bfe9d40193ac24a60b5f716c5
Udana Rathnayaka <rathnayakar07@gmail.com>
2019-05-02 16:30:54 +0530

    initial commit

---
+++ src/DVD.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class DVD {	
+	private List movies=new ArrayList();	
+	public DVD(){}
+	public List getMovies() {
+		return movies;
+	}
+	public void setMovies(List movies) {
+		this.movies = movies;
+	}	
+	public String toString(){
+		String movies="";
+		Movie movie = null;
+		for(Object object:getMovies()){
+			movie = (Movie)object;
+			movies += movie.getName()+", ";
+			movies += movie.getDirectors()+", ";
+			movies += movie.getReleased()+", ";
+			movies += movie.getRuntime()+", ";
+		}
+		return movies; 
+	}	
+	
+	
+ }
